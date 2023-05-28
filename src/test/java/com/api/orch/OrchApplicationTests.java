a6a28279d3e652f6338904a500083595b4353104
Marcos Andrade <marcosjuremao@hotmail.com>
2023-05-28 17:20:06 -0300

    primeiro commit

---
+++ src/test/java/com/api/orch/OrchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.orch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class OrchApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
